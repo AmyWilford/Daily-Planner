3e9acca7b6d758e921fdacf7361a694e38473702
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-22 17:44:36 -0400

    first attempt at time color-coding - bug to be fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,16 @@ let something;
 let dateStatement = moment().format('dddd, MMMM Do YYYY');
 currentDayEl.text(dateStatement);
 
+
+
 // Function to establish hours of day and create and publish relevent rows to <table>
 function publishTableRows() {
+    let currentHour = moment().format('H mm A')
+    console.log(currentHour);
     let hours = [];
     let availableHours = moment('12');
     new Array(19).fill().map((acc, index)=> {
-        hours.push(availableHours.format('h A'))
+        hours.push(availableHours.format('H mm A'))
         availableHours = availableHours.add(1, 'hour');
     })
     for(i = 8; i<hours.length; i++) {
@@ -19,7 +23,18 @@ function publishTableRows() {
         let timeCol = $('<td>').text(hours[i]);
         let activityCol = $('<td>').text('');
         let saveCol = $('<td>').text('');
-        
+        console.log(hours[i]);
+      if (hours[i] < currentHour) {
+        console.log('past')
+        timeCol.css('background-color', 'green')
+      } else if (hours[i] === currentHour) {
+            console.log('right now')
+            timeCol.css('background-color', 'red')
+        } else if (hours[i] > currentHour) {
+                console.log('future');
+            timeCol.css('background-color', 'blue')
+            }
+    
         tableRowEl.append(
             timeCol,
             activityCol,
