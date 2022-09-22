81504163172f34f149932df406d5dd4cdd28fe0c
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-22 17:06:50 -0400

    publishTableRows function created to create table rows for hours of the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 let currentDayEl  = $('#currentDay')
+let tableDisplayEl = $('#table-display')
+let something;
 
 // Show current date
 let dateStatement = moment().format('dddd MMMM, Do YYYY');
 currentDayEl.text(dateStatement);
 
-function publishHours() {
+function publishTableRows() {
     let hours = [];
     let availableHours = moment('12');
     new Array(24).fill().map((acc, index)=> {
@@ -12,9 +14,22 @@ function publishHours() {
         availableHours = availableHours.add(1, 'hour');
     })
     for(i = 0; i<hours.length; i++) {
-        console.log(hours[i] + 'happy');
+        console.log(hours[i]);
+        let tableRowEl = $('<tr>').text('');
+        let timeCol = $('<td>').text(hours[i]);
+        let activityCol = $('<td>').text('');
+        let saveCol = $('<td>').text('');
+        
+        tableRowEl.append(
+            timeCol,
+            activityCol,
+            saveCol
+        );
+        
+        tableDisplayEl.append(tableRowEl);
     }
-    return hours;
+    return 
 }
 
-publishHours();
+publishTableRows();
+
