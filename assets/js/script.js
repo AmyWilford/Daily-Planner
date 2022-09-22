e22f217a1865ff022d87cd3ee01a3dff41e43872
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-22 17:11:24 -0400

    adjusted hours to display working hours only

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,18 @@ let tableDisplayEl = $('#table-display')
 let something;
 
 // Show current date
-let dateStatement = moment().format('dddd MMMM, Do YYYY');
+let dateStatement = moment().format('dddd, MMMM Do YYYY');
 currentDayEl.text(dateStatement);
 
+// Function to establish hours of day and create and publish relevent rows to <table>
 function publishTableRows() {
     let hours = [];
     let availableHours = moment('12');
-    new Array(24).fill().map((acc, index)=> {
+    new Array(19).fill().map((acc, index)=> {
         hours.push(availableHours.format('h A'))
         availableHours = availableHours.add(1, 'hour');
     })
-    for(i = 0; i<hours.length; i++) {
-        console.log(hours[i]);
+    for(i = 8; i<hours.length; i++) {
         let tableRowEl = $('<tr>').text('');
         let timeCol = $('<td>').text(hours[i]);
         let activityCol = $('<td>').text('');
@@ -25,7 +25,7 @@ function publishTableRows() {
             activityCol,
             saveCol
         );
-        
+
         tableDisplayEl.append(tableRowEl);
     }
     return 
